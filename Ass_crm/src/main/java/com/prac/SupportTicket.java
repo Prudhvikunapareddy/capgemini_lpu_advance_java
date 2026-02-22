47d3dba8f17c03bd1d088c10adeb06fcf7f460da
Prudhvi Krishna <princeprudhvi16@gmail.com>
2026-02-22 21:39:28 +0530

    22-02-26

---
+++ Ass_crm/src/main/java/com/prac/SupportTicket.java
@@ -0,0 +1,43 @@
+package com.prac;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class SupportTicket {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String issueDescription;
+
+    @OneToOne
+    private OrderEntity order;
+
+    public SupportTicket(){
+    	
+    }
+
+    public Long getId(){
+    	return id; 
+    	}
+    public void setId(Long id){ 
+    	this.id = id; 
+    	}
+
+    public String getIssueDescription(){ 
+    	return issueDescription; 
+    	}
+    public void setIssueDescription(String issueDescription){
+    	this.issueDescription = issueDescription;
+    	}
+
+    public OrderEntity getOrder(){
+    	return order; 
+    	}
+    public void setOrder(OrderEntity order){ 
+    	this.order = order;
+    	}
+}
